23c2597d2990d89fd37eb79fdd3b5281412e6adc
Gustavo Amorim <guh.contato.work@outlook.com.br>
2021-08-17 19:16:02 -0300

    NodeJs with Express: Second Class

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,25 @@
-import express from 'express'; // const express = require('express');
+import express from "express"; // const express = require('express');
 
 const app = express();
 
-app.listen('3333', () => {
+// Métodos HTTP -> GET | POST | PUT | DELETE
+
+app.get("/users", (request, response) => {
+  return response.json(["usuário 1", "usuário 2"]);
+}); // sem barra no final, uma barra significa que é para receber um parâmetro
+
+app.post("/users", (request, response) => {
+  return response.json({ message: "Criando usuário" });
+});
+
+app.put("/users", (request, response) => {
+  return response.json({ message: "Atualizando usuário" });
+});
+
+app.delete("/users", (request, response) => {
+  return response.json({ message: "Excluindo usuário" });
+});
+
+app.listen("3333", () => {
   console.log("Back-end Started!");
-})
+});
