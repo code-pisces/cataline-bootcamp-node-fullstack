16eec864377a97b4fcac9685d1d737ef3f7669d0
Gustavo Amorim <guh.contato.work@outlook.com.br>
2021-08-18 20:51:20 -0300

    NodeJs with Express: Project Finish

---
+++ README.md
@@ -0,0 +1,29 @@
+<h1 align="center">CRUDAPI</h1>
+
+
+<p align="center">
+  <img alt="Preview" src="./.github/code.png">
+</p>
+
+## ✨ Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- [Node.js](https://nodejs.org/en/)
+- [Typescript](https://www.typescriptlang.org/)
+- [Express](https://expressjs.com/pt-br/)
+
+## 💻 Projeto
+
+Uma API simples como forma de aprender os principais conceitos de uma API REST aplicados ao nodeJS, aguardem o front dessa aplicação.
+
+## 🚀 Como executar
+
+- Clone o repositório
+- Rode `yarn` para baixar as dependências
+- Rode o `yarn dev` para iniciar a aplicação.
+
+Por fim, a aplicação estará disponível em `http://localhost:3333`
+
+---
+Feito com 💜 &nbsp;no bootcamp da Cataline 👋🏻 &nbsp;
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,72 @@
 import express from "express"; // const express = require('express');
+import { v4 as uuid } from "uuid";
 
 const app = express();
+app.use(express.json());
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
+const users: User[] = [];
 
 // Métodos HTTP -> GET | POST | PUT | DELETE
-
 app.get("/users", (request, response) => {
-  return response.json(["usuário 1", "usuário 2"]);
+  // retornar os users
+  return response.json(users);
 }); // sem barra no final, uma barra significa que é para receber um parâmetro
 
 app.post("/users", (request, response) => {
-  return response.json({ message: "Criando usuário" });
+  // receber os dados do novo usuário
+  const { name, email } = request.body;
+
+  // criar novo usuário
+  const user = { id: uuid(), name, email };
+
+  // registrar user na base de dados
+  users.push(user);
+
+  // retorna os dados para quem fez a requisição
+  return response.json(user);
 });
 
-app.put("/users", (request, response) => {
-  return response.json({ message: "Atualizando usuário" });
+app.put("/users/:id", (request, response) => {
+  // receber os dados do usuário
+  const { id } = request.params;
+  const { name, email } = request.body;
+
+  // localizar o usuário na base de dados
+  const userIndex = users.findIndex((user) => user.id === id); // -1
+
+  // se o usuário não existir, retornar um erro
+  if (userIndex < 0) {
+    return response.status(404).json({ error: "User not found." });
+  }
+
+  // retornar o usuário atualizado
+  const user = { id, name, email };
+  users[userIndex] = user;
+
+  return response.json(user);
 });
 
-app.delete("/users", (request, response) => {
-  return response.json({ message: "Excluindo usuário" });
+app.delete("/users/:id", (request, response) => {
+  // receber id do usuário
+  const { id } = request.params;
+
+  // localizar o usuário na base de dados
+  const userIndex = users.findIndex((user) => user.id === id); // -1
+
+  // se o usuário não existir, retornar um erro
+  if (userIndex < 0) {
+    return response.status(404).json({ error: "User not found." });
+  }
+
+  users.splice(userIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.listen("3333", () => {
